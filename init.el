f84ea5835211052edd2bc3b80b8b3e1b0a585abf
sunnyimmortal <1219864007@qq.com>
2021-04-22 23:10:08 +0800

    first question about the auctex

--- init.el
+++ init.el
@@ -1,22 +1,37 @@
 (require 'package)
 (setq package-enable-at-startup nil)
-(add-to-list 'package-archives
-             '("melpa" . "https://melpa.org/packages/"))
+;;(add-to-list 'package-archives
+;;             '("melpa" . "https://melpa.org/packages/"))
+
+  (setq package-archives '(("gnu"   . "http://elpa.emacs-china.org/gnu/")
+                           ("melpa" . "http://elpa.emacs-china.org/melpa/")))
+
 (package-initialize)
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
 
-(org-babel-load-file (expand-file-name "myinit.org"))		   
+(org-babel-load-file "~/.emacs.d/myinit.org")
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-agenda-files
+   (quote
+    ("~/Sync/orgfiles/gcal.org" "~/.emacs.d/myinit.org")))
+ '(org-confirm-babel-evaluate nil)
+ '(org-default-notes-file (concat org-directory "/notes.org"))
+ '(org-directory "~/Sync/orgfiles")
+ '(org-export-html-postamble nil)
+ '(org-hide-leading-stars t)
+ '(org-src-fontify-natively t)
+ '(org-startup-folded (quote overview))
+ '(org-startup-indented t)
  '(package-selected-packages
    (quote
-    (elfeed-org color-theme auto-complete counsel swiper evil eval org-bullets which-key try use-package))))
+    (iedit expand-region hungry-delete beacon yasnippet-snippets htmlize elfeed-org color-theme auto-complete counsel swiper evil eval org-bullets which-key try use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
